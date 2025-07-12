9095344d6066f4f8611eba44515e626ff4d64df7
Pedro Severin <ppseverin@miuandes.cl>
2025-07-12 14:09:00 -0400

    [FIX] Correct package name from 'pynet' to 'finpy' in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pynet',
+    name='finpy',
     version='0.1.18',
     author='Pedro Severin',
     author_email='ppseverin@miuandes.cl',
